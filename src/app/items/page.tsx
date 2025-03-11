df129275eecf77ca066d0c6f047a8020cbfa7791
노수민 <nosumin29@gmail.com>
2025-03-11 22:20:27 +0900

    🎉 Add: 아이템 리스트 페칭 CSR방식에서 SSG방식으로 변경

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,13 +1,20 @@
 import ItemsList from "@/components/items/ItemsList";
 import MainWrapper from "@/ui/MainWrapper";
 import Title from "@/ui/Title";
+import { fetchItemsList } from "@/utils/serverApi";
 import React from "react";
 
-const ItemsPage = () => {
+const ItemsPage = async () => {
+  const items = await fetchItemsList();
+
   return (
     <MainWrapper>
       <Title>아이템 목록</Title>
-      <ItemsList />
+      {items?.length ? (
+        <ItemsList data={items} />
+      ) : (
+        <div>아이템 데이터를 불러올 수 없습니다.</div>
+      )}
     </MainWrapper>
   );
 };
--- src/components/items/ItemsList.tsx
+++ src/components/items/ItemsList.tsx
@@ -1,25 +1,17 @@
 "use client";
-import Loading from "@/app/loading";
 import { Item } from "@/types/Item";
 import CardWrapper from "@/ui/CardWrapper";
-import { fetchItemsList } from "@/utils/serverApi";
-import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import ItemCard from "./ItemCard";
 
-const ItemsList = () => {
-  const { data, isFetching, error } = useQuery({
-    queryKey: ["items"],
-    queryFn: fetchItemsList,
-  });
-  if (isFetching) return <Loading />;
-  if (error) return <div>error occur</div>;
+type ItemList = {
+  data: Item[];
+};
 
+const ItemsList = ({ data }: ItemList) => {
   const itemsListArr: Item[] = data
     ? (Object.values(data) as Item[]).map((e) => e as Item)
     : [];
-
-  console.log(itemsListArr);
 
   return (
     <CardWrapper>
