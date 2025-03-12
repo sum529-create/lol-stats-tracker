bc7638a5ee4e745e6e3e97d8bc987b71ed1101f9
노수민 <nosumin29@gmail.com>
2025-03-12 17:59:24 +0900

    ✨ Update: 챔피언 로테이션 페이지에 신규 플레이어를 위한 무료 챔피언 목록 추가 및 RotationList 컴포넌트에 타입 매개변수 추가

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -7,7 +7,11 @@ const RotationPage = () => {
   return (
     <MainWrapper>
       <Title>챔피언 로테이션 (이번주 무료로 플레이 할 수 있어요!)</Title>
-      <RotationList />
+      <RotationList type="freeChampionIds" />
+      <div className="mt-25">
+        <Title>신규 플레이어를 위한 무료 챔피언 로테이션</Title>
+        <RotationList type="freeChampionIdsForNewPlayers" />
+      </div>
     </MainWrapper>
   );
 };
