4cdc1de560c49a5e6c77737ad3dc78a77c4ff02b
노수민 <nosumin29@gmail.com>
2025-03-12 19:44:11 +0900

    ✨ Update: 챔피언 목록 페이지에서 데이터 패칭 방식 변경

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,14 +1,19 @@
-"use client";
-
 import ChampionsList from "@/components/champions/ChampionsList";
+import { ChampionBasic } from "@/types/Champion";
 import MainWrapper from "@/ui/MainWrapper";
 import Title from "@/ui/Title";
+import { fetchChampionList } from "@/utils/serverApi";
 
-const ChampionsPage = () => {
+const ChampionsPage = async () => {
+  const data = await fetchChampionList();
+  const championsList: ChampionBasic[] = data
+    ? Object.values(data).map((e) => e as ChampionBasic)
+    : [];
+
   return (
     <MainWrapper>
       <Title>챔피언 목록</Title>
-      <ChampionsList />
+      <ChampionsList championsList={championsList} />
     </MainWrapper>
   );
 };
--- src/components/champions/ChampionsList.tsx
+++ src/components/champions/ChampionsList.tsx
@@ -1,28 +1,14 @@
+"use client";
 import React from "react";
 import ChampionsCard from "./ChampionsCard";
 import { ChampionBasic } from "@/types/Champion";
-import { fetchChampionList } from "@/utils/serverApi";
-import { useQuery } from "@tanstack/react-query";
-import Loading from "@/app/loading";
 import CardWrapper from "@/ui/CardWrapper";
 
-const ChampionsList = () => {
-  const { data, isFetching, isError } = useQuery({
-    queryKey: ["champions"],
-    queryFn: fetchChampionList,
-  });
-
-  if (isFetching) {
-    return <Loading />;
-  }
-  if (isError) {
-    return <div>Error loading champions.</div>;
-  }
-
-  const championsList: ChampionBasic[] = data
-    ? Object.values(data).map((e) => e as ChampionBasic)
-    : [];
-
+const ChampionsList = ({
+  championsList,
+}: {
+  championsList: ChampionBasic[];
+}) => {
   return (
     <CardWrapper>
       {championsList?.map((champion, i) => (
